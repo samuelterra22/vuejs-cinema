e2a58847c377454e5544482dbc76679b903a6469
Samuel Terra <samuelterra22@hotmail.com>
2018-04-11 18:08:47 -0300

    Termina a lógica e implementação dos filtros

--- src/main.js
+++ src/main.js
@@ -5,29 +5,54 @@ import genres from './util/genres'
 
 new Vue({
   el: '#app',
-  methods:{
-    checkFilter(category, title, checked){
-      console.log(category, title, checked)
-    }
+  data: {
+    genre: [],
+    time: []
   },
-  components:{
-    'movie-list': {
-      template: `<div id="movie-list">
-                    <div v-for="movie in movies" class="movie">{{movie.title}}</div>
-                </div>`,
-      data() {
-        return {
-          movies:[
-            {title: 'Pulp Fiction'},
-            {title: 'Home alone'},
-            {title: 'Austin Powers'},
-          ]
+  methods: {
+    checkFilter (category, title, checked) {
+      if (checked) {
+        this[category].push(title)
+      } else {
+        let index = this[category].indexOf(title)
+        if (index > -1) {
+          this[category].splice(index, 1)
         }
       }
-
+    }
+  },
+  components: {
+    'movie-list': {
+      template: `<div id="movie-list">
+                    <div v-for="movie in filteredMovies" class="movie">{{movie.title}}</div>
+                </div>`,
+      data () {
+        return {
+          movies: [
+            {title: 'Pulp Fiction', genre: genres.CRIME},
+            {title: 'Home alone', genre: genres.COMEDY},
+            {title: 'Austin Powers', genre: genres.COMEDY},
+          ]
+        }
+      },
+      props: ['genre', 'title'],
+      methods:{
+        moviePassesGenreFilter(movie){
+          if (!this.genre.length) {
+            return true
+          }else{
+            return this.genre.find(genre => movie.genre === genre)
+          }
+        }
+      },
+      computed: {
+        filteredMovies () {
+          return this.movies.filter(this.moviePassesGenreFilter)
+        }
+      }
     },
     'movie-filter': {
-      data(){
+      data () {
         return {
           genres
         }
@@ -38,14 +63,14 @@ new Vue({
                         <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
                     </div>
                 </div>`,
-      methods:{
-        checkFilter(category, title, checked){
+      methods: {
+        checkFilter (category, title, checked) {
           this.$emit('check-filter', category, title, checked)
         }
       },
-      components:{
-        'check-filter':{
-          data(){
+      components: {
+        'check-filter': {
+          data () {
             return {
               checked: false
             }
@@ -55,8 +80,8 @@ new Vue({
                         <span class="checkbox"></span>
                         <span class="check-filter-title">{{title}}</span>
                     </div>`,
-          methods:{
-            checkFilter(){
+          methods: {
+            checkFilter () {
               this.checked = !this.checked
               this.$emit('check-filter', 'genre', this.title, this.checked)
             }
