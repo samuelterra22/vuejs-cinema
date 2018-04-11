5fcb00c9a239805f6bf7f7afe0500fc72833986f
Samuel Terra <samuelterra22@hotmail.com>
2018-04-11 17:49:28 -0300

    Adiciona logica de eventos do filtro

---
+++ src/main.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+import './style.scss'
+
+import genres from './util/genres'
+
+new Vue({
+  el: '#app',
+  methods:{
+    checkFilter(category, title, checked){
+      console.log(category, title, checked)
+    }
+  },
+  components:{
+    'movie-list': {
+      template: `<div id="movie-list">
+                    <div v-for="movie in movies" class="movie">{{movie.title}}</div>
+                </div>`,
+      data() {
+        return {
+          movies:[
+            {title: 'Pulp Fiction'},
+            {title: 'Home alone'},
+            {title: 'Austin Powers'},
+          ]
+        }
+      }
+
+    },
+    'movie-filter': {
+      data(){
+        return {
+          genres
+        }
+      },
+      template: `<div id="movie-filter">
+                    <h2>Movie filter</h2>
+                    <div class="filter-group">
+                        <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+                    </div>
+                </div>`,
+      methods:{
+        checkFilter(category, title, checked){
+          this.$emit('check-filter', category, title, checked)
+        }
+      },
+      components:{
+        'check-filter':{
+          data(){
+            return {
+              checked: false
+            }
+          },
+          props: ['title'],
+          template: `<div v-bind:class="{'check-filter' : true, active: checked}" v-on:click="checkFilter">
+                        <span class="checkbox"></span>
+                        <span class="check-filter-title">{{title}}</span>
+                    </div>`,
+          methods:{
+            checkFilter(){
+              this.checked = !this.checked
+              this.$emit('check-filter', 'genre', this.title, this.checked)
+            }
+          }
+        }
+      }
+    }
+  }
+})
