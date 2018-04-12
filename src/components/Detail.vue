47a819e00e6709fb1be4799416d8f148047b0f7d
Samuel Terra <samuelterra22@hotmail.com>
2018-04-12 16:49:06 -0300

    Aplica conseitos de slots e refatora os components

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,12 +1,26 @@
 <template>
-    <div>
-        Hello World
+    <div id="detail" v-if="movie">
+        <movie-item v-bind:movie="movie.movie"></movie-item>
     </div>
 </template>
 
 <script>
-  export default {
+  import MovieItem from './MovieItem.vue'
 
+  export default {
+    components: {
+      MovieItem
+    },
+    props: ['movies'],
+    computed: {
+      movie () {
+        let movie = this.movies.find(movie => movie.id === this.$route.params.id)
+        return movie ? movie : null
+      }
+    },
+    created () {
+      console.log(this.$route.params.id)
+    },
   }
 </script>
 
