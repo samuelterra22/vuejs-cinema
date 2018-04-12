aad6237fc074d7a02bd6c9d7c6311da7cbe19bcd
Samuel Terra <samuelterra22@hotmail.com>
2018-04-12 15:56:27 -0300

    Adiciona router-view no template principal

--- src/main.js
+++ src/main.js
@@ -1,42 +1,37 @@
 import Vue from 'vue'
 import './style.scss'
 
-import Overview from './components/Overview.vue'
-
 import VueResource from 'vue-resource'
+import moment from 'moment-timezone'
+import { checkFilter } from './util/bus'
+import VueRouter from 'vue-router'
+import routes from './util/routes'
+
 Vue.use(VueResource)
 
-import moment from 'moment-timezone'
 moment.tz.setDefault('UTC')
-Object.defineProperty(Vue.prototype, '$moment', {get() { return this.$root.moment} })
-
-import {checkFilter} from './util/bus'
+Object.defineProperty(Vue.prototype, '$moment', {get () { return this.$root.moment}})
 
 const bus = new Vue()
-Object.defineProperty(Vue.prototype, '$bus', {get(){return this.$root.bus}})
+Object.defineProperty(Vue.prototype, '$bus', {get () {return this.$root.bus}})
 
-import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
-import routes from './util/routes'
-const router = new VueRouter({ routes })
+const router = new VueRouter({routes})
 
 new Vue({
   el: '#app',
   data: {
     genre: [],
     time: [],
-    movies:[],
+    movies: [],
     moment,
     day: moment(),
     bus
   },
-  components: {
-    Overview
-  },
-  created(){
+  created () {
     this.$http.get('/api')
-      .then(response =>{
+      .then(response => {
         this.movies = response.data
       })
     this.$bus.$on('check-filter', checkFilter.bind(this))
