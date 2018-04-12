ee8b87fab3190fe2ce2e696f7c109cfaff4b4ad4
Samuel Terra <samuelterra22@hotmail.com>
2018-04-11 21:43:51 -0300

    Realiza separação dos componentes

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="movie-filter">
+        <h2>Movie filter</h2>
+        <div class="filter-group">
+            <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+        </div>
+    </div>
+</template>
+<script>
+  import genres from '../util/genres';
+
+  import CheckFilter from '../components/CheckFilter.vue';
+
+  export default {
+    data () {
+      return {
+        genres
+      }
+    },
+    methods: {
+      checkFilter (category, title, checked) {
+        this.$emit('check-filter', category, title, checked)
+      }
+    },
+    components: {
+      CheckFilter
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
