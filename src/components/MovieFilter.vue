6a3df3ed9ee199e4b41b63c8821ef24d1a5c8476
Samuel Terra <samuelterra22@hotmail.com>
2018-04-12 12:00:22 -0300

    Adiciona filtro de horario das sessões

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,20 +1,27 @@
 <template>
     <div id="movie-filter">
         <h2>Movie filter</h2>
+        <h3>By time of day</h3>
         <div class="filter-group">
-            <check-filter v-for="genre in genres" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
+            <check-filter v-for="time in times" category="time" v-bind:title="time" v-on:check-filter="checkFilter"></check-filter>
+        </div>
+        <h3>By genre</h3>
+        <div class="filter-group">
+            <check-filter v-for="genre in genres" category="genre" v-bind:title="genre" v-on:check-filter="checkFilter"></check-filter>
         </div>
     </div>
 </template>
 <script>
   import genres from '../util/genres';
+  import times from '../util/times';
 
   import CheckFilter from '../components/CheckFilter.vue';
 
   export default {
     data () {
       return {
-        genres
+        genres,
+        times
       }
     },
     methods: {
